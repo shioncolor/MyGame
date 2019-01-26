e27f16f21c6a7bf89da696c29ce2a45625b349ca
tyaool <czamy00@gmail.com>
2019-01-27 01:38:30 +0900

    バグ修正

--- Assets/script/Game/MainGame/BirdView/BirdViewMainGame.cs
+++ Assets/script/Game/MainGame/BirdView/BirdViewMainGame.cs
@@ -31,7 +31,7 @@ public class BirdViewMainGame : MonoBehaviour {
     }
     // Update is called once per frame
     void Update () {
-     //  Timar();
+      Timar();
 
         if (NowTime >= TimeRimit)
         {
